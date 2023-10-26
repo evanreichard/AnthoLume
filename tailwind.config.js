39c78997ac1f6b4a141cca0af6e83ea14a2340b6
Evan Reichard <evan@reichard.io>
2023-10-25 20:08:09 -0400

    [fix] regression css style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.{html,js}"],
+  content: ["./templates/**/*.html", "./assets/reader/index.js"],
   theme: {
     extend: {},
   },
