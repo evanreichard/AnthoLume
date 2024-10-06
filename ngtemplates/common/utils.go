3cf374ea2b8fd53d0611f8e63956194a56579a0e
Evan Reichard <evan@reichard.io>
2024-10-06 17:01:37 -0400

    templ wip 2

--- ngtemplates/common/utils.go
+++ ngtemplates/common/utils.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	"fmt"
+	"math"
 	"strings"
 )
 
@@ -38,4 +40,112 @@ type Settings struct {
 	Version       string
 	IsAdmin       bool
 	SearchEnabled bool
+}
+
+type UserMetadata struct {
+	DocumentCount int
+	ActivityCount int
+	ProgressCount int
+	DeviceCount   int
+}
+
+type UserStatistics struct {
+	WPM      map[string][]UserStatisticEntry
+	Duration map[string][]UserStatisticEntry
+	Words    map[string][]UserStatisticEntry
+}
+
+type UserStatisticEntry struct {
+	UserID string
+	Value  string
+}
+
+// getTimeZones returns a string slice of IANA timezones.
+func GetTimeZones() []string {
+	return []string{
+		"Africa/Cairo",
+		"Africa/Johannesburg",
+		"Africa/Lagos",
+		"Africa/Nairobi",
+		"America/Adak",
+		"America/Anchorage",
+		"America/Buenos_Aires",
+		"America/Chicago",
+		"America/Denver",
+		"America/Los_Angeles",
+		"America/Mexico_City",
+		"America/New_York",
+		"America/Nuuk",
+		"America/Phoenix",
+		"America/Puerto_Rico",
+		"America/Sao_Paulo",
+		"America/St_Johns",
+		"America/Toronto",
+		"Asia/Dubai",
+		"Asia/Hong_Kong",
+		"Asia/Kolkata",
+		"Asia/Seoul",
+		"Asia/Shanghai",
+		"Asia/Singapore",
+		"Asia/Tokyo",
+		"Atlantic/Azores",
+		"Australia/Melbourne",
+		"Australia/Sydney",
+		"Europe/Berlin",
+		"Europe/London",
+		"Europe/Moscow",
+		"Europe/Paris",
+		"Pacific/Auckland",
+		"Pacific/Honolulu",
+	}
+}
+
+// niceSeconds takes in an int (in seconds) and returns a string readable
+// representation. For example 1928371 -> "22d 7h 39m 31s".
+func NiceSeconds(input int64) (result string) {
+	if input == 0 {
+		return "N/A"
+	}
+
+	days := math.Floor(float64(input) / 60 / 60 / 24)
+	seconds := input % (60 * 60 * 24)
+	hours := math.Floor(float64(seconds) / 60 / 60)
+	seconds = input % (60 * 60)
+	minutes := math.Floor(float64(seconds) / 60)
+	seconds = input % 60
+
+	if days > 0 {
+		result += fmt.Sprintf("%dd ", int(days))
+	}
+	if hours > 0 {
+		result += fmt.Sprintf("%dh ", int(hours))
+	}
+	if minutes > 0 {
+		result += fmt.Sprintf("%dm ", int(minutes))
+	}
+	if seconds > 0 {
+		result += fmt.Sprintf("%ds", int(seconds))
+	}
+
+	return
+}
+
+// niceNumbers takes in an int and returns a string representation. For example
+// 19823 -> "19.8k".
+func NiceNumbers(input int64) string {
+	if input == 0 {
+		return "0"
+	}
+
+	abbreviations := []string{"", "k", "M", "B", "T"}
+	abbrevIndex := int(math.Log10(float64(input)) / 3)
+	scaledNumber := float64(input) / math.Pow(10, float64(abbrevIndex*3))
+
+	if scaledNumber >= 100 {
+		return fmt.Sprintf("%.0f%s", scaledNumber, abbreviations[abbrevIndex])
+	} else if scaledNumber >= 10 {
+		return fmt.Sprintf("%.1f%s", scaledNumber, abbreviations[abbrevIndex])
+	} else {
+		return fmt.Sprintf("%.2f%s", scaledNumber, abbreviations[abbrevIndex])
+	}
 }
